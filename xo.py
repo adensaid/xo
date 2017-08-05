63e82b850b79c44a70851ffc5e387bbdd11b0b83
Alex Florea <30704951+AlexMFlorea@users.noreply.github.com>
2017-08-05 12:44:02 +0100

    add first version of main script

---
+++ xo.py
@@ -0,0 +1,18 @@
+""" An unbeatable noughts and crosses AI"""
+import sys
+
+
+def main():
+    """Main entry point"""
+    print("Welcome to XO AI!")
+    print("©2017 AF & SA ltd. ")
+    print("All rights reserved")
+
+    print()
+
+    print(" |   |   |   |")
+    print(" |   |   |   |")
+    print(" |   |   |   |")
+
+if __name__ == '__main__':
+    sys.exit(main())
